266617b572fc80c05f2b51d50651dd20e004ba1a
ModderCodder <1modder.path1@gmail.com>
2026-03-30 13:03:13 +0300

    __init__.py import fix

--- server_app/schemas/__init__.py
+++ server_app/schemas/__init__.py
@@ -1,6 +1,4 @@
-from .user_schemas import UserCreate, UserResponse, UserUpdate
-from .chat_schemas import ChatCreate, ChatResponse, ChatUpdate
-from .message_schemas import MessageCreate, MessageResponse
-__all__ = ["UserBase", "UserCreate", "UserResponse", "UserUpdate",
-           "ChatCreate", "ChatResponse", "ChatUpdate",
-           "MessageCreate", "MessageResponse"]
+from .user_schemas import UserResponse, UserUpdate, UserLogin, UserRegister
+
+
+__all__ = ["UserResponse", "UserUpdate", "UserLogin", "UserRegister"]
